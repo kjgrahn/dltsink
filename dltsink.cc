b18a7ea4141a8498d02e8e761163e59deff437fe
Jörgen Grahn <grahn+src@snipabacken.se>
2026-01-29 22:50:43 +0100

    dltsink 1.1

--- dltsink.cc
+++ dltsink.cc
@@ -227,7 +227,7 @@ int main(int argc, char ** argv)
 	    arg.filename = optarg;
 	    break;
 	case 'V':
-	    std::cout << prog << " 1.0\n"
+	    std::cout << prog << " 1.1\n"
 		      << "Copyright (c) 2026 Jörgen Grahn\n";
 	    return 0;
 	    break;
